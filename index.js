e2f90e3fac4d01b7123b68bb635dcbac9ddce206
ShaneDsouza4 <shane.json96@gmail.com>
2024-07-13 23:49:46 -0400

    socket installed on backend

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const http = require('http'); //http module to attach WS
 const express = require('express');
 const path = require("path");
+const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
+const io = new Server(server); //Handles sockets
 
 app.use(express.static(path.resolve("./public")))
 
+// Socket 
+io.on('connection', (socket) => {
+    console.log("New user has connected", socket.id);
+})
+
+
+
+//HTTP Requests
 app.get("/", (req, res)=>{
     return res.sendFile("/public/index.html")
 })
