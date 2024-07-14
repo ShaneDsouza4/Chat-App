6451ff6efd5c7b08f16dc889f3a9da27b9750c11
ShaneDsouza4 <shane.json96@gmail.com>
2024-07-13 23:39:50 -0400

    Sending HTML file on /

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const http = require('http'); //http module to attach WS
 const express = require('express');
+const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
 
-server.listen(800, ()=>console.log("Server running at PORT:8000"));
+app.use(express.static(path.resolve("./public")))
+
+app.get("/", (req, res)=>{
+    return res.sendFile("/public/index.html")
+})
+
+server.listen(8000, ()=>console.log("Server running at PORT:8000"));
  
